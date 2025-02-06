40301fdc0436e94b141f0823e0fa559980e0e2f1
Łukasz Mieczkowski <lmieczkowski@grupazpr.pl>
2025-02-06 09:06:20 +0100

    minor: code formatting

--- python/playwright-hello-world/playwright_hello_world.py
+++ python/playwright-hello-world/playwright_hello_world.py
@@ -13,9 +13,11 @@ if __name__ == "__main__":
         description="""Very simple example of the 'playwright' lib usage. 
         It visits the 'projekty.muratordom.pl' page and adds first project to the shopping cart."""
     )
-    # Playwright's engine browser must be installed first: 
+    # Playwright's engine browser must be installed first:
     # `python -m playwright install`
-    args_parser.add_argument("browser", help=f"Playwright's browser engine: {SUPPORTED_BROWSERS_INFO}.")
+    args_parser.add_argument(
+        "browser", help=f"Playwright's browser engine: {SUPPORTED_BROWSERS_INFO}."
+    )
     args = args_parser.parse_args()
 
     sanitized_browser_engine_name = args.browser.strip().lower()
@@ -23,7 +25,7 @@ if __name__ == "__main__":
     if sanitized_browser_engine_name not in SUPPORTED_BROWSERS:
         print(f"Invalid browser name. Supported browsers: {SUPPORTED_BROWSERS_INFO}")
         sys.exit(1)
-    
+
     with sync_playwright() as playwright:
         engine = getattr(playwright, sanitized_browser_engine_name)
         print(engine)
