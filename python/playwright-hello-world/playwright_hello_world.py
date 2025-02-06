a0248f7d8dd95f926be48c317d3377fcc1ee511b
Łukasz Mieczkowski <lmieczkowski@grupazpr.pl>
2025-02-06 09:04:48 +0100

    Browser engine selection

--- python/playwright-hello-world/playwright_hello_world.py
+++ python/playwright-hello-world/playwright_hello_world.py
@@ -1,25 +1,29 @@
 import argparse
+import sys
 
 from playwright.sync_api import sync_playwright
 
 TARGET_URL = "https://projekty.muratordom.pl/"
+
+SUPPORTED_BROWSERS = ["webkit", "chromium", "firefox"]
+SUPPORTED_BROWSERS_INFO = str.join("|", SUPPORTED_BROWSERS)
 
 if __name__ == "__main__":
     args_parser = argparse.ArgumentParser(
         description="""Very simple example of the 'playwright' lib usage. 
         It visits the 'projekty.muratordom.pl' page and adds first project to the shopping cart."""
     )
-
     # Playwright's engine browser must be installed first: 
     # `python -m playwright install`
-    args_parser.add_argument("browser", help="Playwright's browser engine: webkit | chromium | firefox.")
-    
+    args_parser.add_argument("browser", help=f"Playwright's browser engine: {SUPPORTED_BROWSERS_INFO}.")
     args = args_parser.parse_args()
+
+    sanitized_browser_engine_name = args.browser.strip().lower()
+
+    if sanitized_browser_engine_name not in SUPPORTED_BROWSERS:
+        print(f"Invalid browser name. Supported browsers: {SUPPORTED_BROWSERS_INFO}")
+        sys.exit(1)
     
     with sync_playwright() as playwright:
-        playwright.chromium
-
-
-# TODO
-# 1. Możliwość wyboru 3 różnych silników z linii poleceń.
-#   1.1 Parsowanie argumentu wejściowego za pomocą argparse (z prostą walidacją)
+        engine = getattr(playwright, sanitized_browser_engine_name)
+        print(engine)
